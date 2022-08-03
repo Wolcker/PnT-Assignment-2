a6f7982124a566c3c9a35bb06fe5c06c871ed741
Leonard Wolcker <leonard@plug-n-trade.com>
2022-08-03 10:01:58 +0200

    Fixed the WOrking Schedule.cs

--- Assignment2/WorkingSchedule.cs
+++ Assignment2/WorkingSchedule.cs
@@ -8,7 +8,56 @@ namespace Assignment2
     {
         internal static void Start()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("YOUR SCHEDULE PROGRAM");
+            Console.WriteLine("Select from the menu which type of scheduale you want to see.");
+            Console.WriteLine("");
+            Console.WriteLine("1 Show a list of the weekends to work.");
+            Console.WriteLine("2 Show a list of nights to work.");
+            Console.WriteLine("0 Return to the Main Menu.");
+            Console.WriteLine("");
+            Console.Write("Your choice: ");
+            int number = int.Parse(Console.ReadLine());
+            ScheduleListSwitch(number);
+
+        }
+
+        private static void ScheduleListSwitch(int number)
+        {
+            switch(number) {
+                case 1:
+                    weekendSchedule();
+                    Start();
+                    break;
+                case 2:
+                    nightSchedule();
+                    Start();
+                    break;
+                case 0: 
+                    break;
+                default:
+                    Console.WriteLine("Please choose a CORRECT number from the list.");
+                    Start();
+                    break;
+            }
+        }
+
+        private static void nightSchedule()
+        {
+            for(int i=6; i <= 52; i=i+5)
+            {
+                Console.Write("          Week " +i);
+            }
+            Console.WriteLine("");
+        }
+
+        private static void weekendSchedule()
+        {
+            for (int i = 1; i <= 52; i = i + 3)
+            {
+                Console.Write("          Week " + i);
+            }
+            Console.WriteLine("");
         }
     }
 }
